425fcb1afa17b76321f2e1446ee98feb73feb84f
Raghav Mishra <mishraraghav24@gmail.com>
2025-02-26 12:11:43 +1100

    Create python package with brax environment

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='reach_mjx',
+    author="Raghav Mishra",
+    author_email="r.mishra@acfr.usyd.edu.au",
+    packages=find_packages(),
+    version='0.0.1',
+)
